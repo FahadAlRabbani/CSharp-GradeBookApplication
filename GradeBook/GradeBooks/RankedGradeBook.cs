4404fa0e462d4bfdc08c73f4e2006e7587074105
Fahad Al Rabbani Jillani <FahadAlRabbani@users.noreply.github.com>
2021-02-02 17:33:04 +0000

    Update RankedGradeBook.cs
    Add algorithm to calculate Ranked Grades and return a letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,23 @@ namespace GradeBook.GradeBooks
         {
             this.Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            var output = (averageGrade * Students.Count) / 100;
+
+            if (output >= 20.0)
+                return 'A';
+            if (output >= 40.0 && output < 20.0)
+                return 'B';
+            if (output >= 60.0 && output < 40.0)
+                return 'C';
+            if (output >= 80.0 && output < 60.0)
+                return 'D';
+
+            return 'F';
+        }
     }
 }
