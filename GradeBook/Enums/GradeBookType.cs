e375750550132d1a8ceb569092ddb564569b517b
Fahad Al Rabbani Jillani <FahadAlRabbani@users.noreply.github.com>
2021-02-07 19:11:10 +0000

    Update Create Command

--- GradeBook/Enums/GradeBookType.cs
+++ GradeBook/Enums/GradeBookType.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace GradeBook.Enums
 {
     public enum GradeBookType
     {
+        [Description("standard")]
         Standard = 0,
+        [Description("ranked")]
         Ranked,
+        [Description("esnu")]
         ESNU,
+        [Description("onetofour")]
         OneToFour,
+        [Description("sixpoint")]
         SixPoint
     }
 }
